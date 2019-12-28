51f2ea0f49adbd2d0dcea07fdeecad64829af064
Viktor Sharepo <shedelok.1@gmail.com>
2019-12-29 00:59:29 +0300

    Added Cht.cpp

---
+++ Cht.cpp
@@ -0,0 +1,50 @@
+class Cht {
+    vector<pair<long long, long long>> lines;
+    vector<long double> intersections;
+
+public:
+    void addLine(long long k, long long b) {
+        pair<long long, long long> line = {k, b};
+        if (!lines.empty() && k == lines.back().first) {
+            if (b >= lines.back().second) {
+                return;
+            }
+            lines.pop_back();
+            if (!intersections.empty()) {
+                intersections.pop_back();
+            }
+        }
+        while (lines.size() > 1) {
+            long double x = intersections[lines.size() - 2];
+            if (1. * line.first * x + line.second <= 1. * lines.back().first * x + lines.back().second) {
+                lines.pop_back();
+                intersections.pop_back();
+            } else {
+                break;
+            }
+        }
+        if (!lines.empty()) {
+            intersections.push_back(1. * (lines.back().second - line.second) / (line.first - lines.back().first));
+        }
+        lines.push_back(line);
+    }
+
+    long long getMin(long long x) const {
+        if (lines.size() == 1) {
+            return 1LL * lines[0].first * x + lines[0].second;
+        }
+        int bl = 1;
+        int br = lines.size() - 1;
+        int bRes = 0;
+        while (bl <= br) {
+            int mid = (bl + br) / 2;
+            if (intersections[mid - 1] <= x) {
+                bRes = mid;
+                bl = mid + 1;
+            } else {
+                br = mid - 1;
+            }
+        }
+        return 1LL * lines[bRes].first * x + lines[bRes].second;
+    }
+};
