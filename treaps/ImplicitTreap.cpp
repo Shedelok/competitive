fea182e2ffbfba56457b03ac74c8f4e2261baf97
Viktor Sharepo <shedelok.1@gmail.com>
2020-03-17 21:39:32 +0300

    Made all methods static inside Node

--- treaps/ImplicitTreap.cpp
+++ treaps/ImplicitTreap.cpp
@@ -1,7 +1,7 @@
 namespace ImplicitTreap {
-    mt19937 rnd(11);
-
     struct Node {
+        static mt19937 rnd;
+
         const int y;
         Node *leftChild;
         Node *rightChild;
@@ -10,91 +10,93 @@ namespace ImplicitTreap {
         int sz;
 
         explicit Node(int value) : y(rnd()), leftChild(nullptr), rightChild(nullptr), value(value), sz(1) {}
+
+        static Node *getNewRoot() {
+            return nullptr;
+        }
+
+        static bool isEmptyTree(Node *node) {
+            return node == nullptr;
+        }
+
+        static int getSz(Node *node) {
+            return isEmptyTree(node) ? 0 : node->sz;
+        }
+
+        static void push(Node *node) {
+            if (isEmptyTree(node)) {
+                return;
+            }
+        }
+
+        static void update(Node *node) {
+            if (isEmptyTree(node)) {
+                return;
+            }
+            node->sz = 1 + getSz(node->leftChild) + getSz(node->rightChild);
+        }
+
+        static Node *merge(Node *smaller, Node *bigger) {
+            if (isEmptyTree(smaller)) {
+                return bigger;
+            }
+            if (isEmptyTree(bigger)) {
+                return smaller;
+            }
+            push(smaller);
+            push(bigger);
+            Node *result;
+            if (smaller->y > bigger->y) {
+                result = smaller;
+                result->rightChild = merge(smaller->rightChild, bigger);
+            } else {
+                result = bigger;
+                result->leftChild = merge(smaller, bigger->leftChild);
+            }
+            update(result);
+            return result;
+        }
+
+        static pair<Node *, Node *> split(Node *node, int key) {
+            if (isEmptyTree(node)) {
+                return make_pair(getNewRoot(), getNewRoot());
+            }
+            push(node);
+            int pos = 1 + getSz(node->leftChild);
+            if (pos <= key) {
+                auto rec = split(node->rightChild, key - pos);
+                node->rightChild = rec.first;
+                update(node);
+                return make_pair(node, rec.second);
+            } else {
+                auto rec = split(node->leftChild, key);
+                node->leftChild = rec.second;
+                update(node);
+                return make_pair(rec.first, node);
+            }
+        }
+
+        static vector<Node *> multiSplit(Node *node, const initializer_list<int> &keys) {
+            vector<Node *> result = {node};
+            result.reserve(keys.size() + 1);
+            int sum = 0;
+            for (auto key : keys) {
+                auto p = split(result.back(), key - sum);
+                sum += key;
+                result.back() = p.first;
+                result.push_back(p.second);
+            }
+            return result;
+        }
+
+        static Node *multiMerge(const initializer_list<Node *> nodes) {
+            auto result = getNewRoot();
+            for (auto node : nodes) {
+                result = merge(result, node);
+            }
+            return result;
+        }
     };
 
-    Node *getNewRoot() {
-        return nullptr;
-    }
-
-    bool isEmptyTree(Node *node) {
-        return node == nullptr;
-    }
-
-    int getSz(Node *node) {
-        return isEmptyTree(node) ? 0 : node->sz;
-    }
-
-    void push(Node *node) {
-        if (isEmptyTree(node)) {
-            return;
-        }
-    }
-
-    void update(Node *node) {
-        if (isEmptyTree(node)) {
-            return;
-        }
-        node->sz = 1 + getSz(node->leftChild) + getSz(node->rightChild);
-    }
-
-    Node *merge(Node *smaller, Node *bigger) {
-        if (isEmptyTree(smaller)) {
-            return bigger;
-        }
-        if (isEmptyTree(bigger)) {
-            return smaller;
-        }
-        push(smaller);
-        push(bigger);
-        Node *result;
-        if (smaller->y > bigger->y) {
-            result = smaller;
-            result->rightChild = merge(smaller->rightChild, bigger);
-        } else {
-            result = bigger;
-            result->leftChild = merge(smaller, bigger->leftChild);
-        }
-        update(result);
-        return result;
-    }
-
-    pair<Node *, Node *> split(Node *node, int key) {
-        if (isEmptyTree(node)) {
-            return make_pair(getNewRoot(), getNewRoot());
-        }
-        push(node);
-        int pos = 1 + getSz(node->leftChild);
-        if (pos <= key) {
-            auto rec = split(node->rightChild, key - pos);
-            node->rightChild = rec.first;
-            update(node);
-            return make_pair(node, rec.second);
-        } else {
-            auto rec = split(node->leftChild, key);
-            node->leftChild = rec.second;
-            update(node);
-            return make_pair(rec.first, node);
-        }
-    }
-
-    vector<Node *> multiSplit(Node *node, const initializer_list<int> &keys) {
-        vector<Node *> result = {node};
-        result.reserve(keys.size() + 1);
-        int sum = 0;
-        for (auto key : keys) {
-            auto p = split(result.back(), key - sum);
-            sum += key;
-            result.back() = p.first;
-            result.push_back(p.second);
-        }
-        return result;
-    }
-
-    Node *multiMerge(const initializer_list<Node *> nodes) {
-        auto result = getNewRoot();
-        for (auto node : nodes) {
-            result = merge(result, node);
-        }
-        return result;
-    }
+    mt19937 Node::rnd(11);
 }
--- treaps/Treap.cpp
+++ treaps/Treap.cpp
@@ -1,90 +1,92 @@
 namespace Treap {
-    mt19937 rnd(11);
-
     struct Node {
+        static mt19937 rnd;
+
         int x;
         const int y;
         Node *leftChild;
         Node *rightChild;
 
         explicit Node(int x) : x(x), y(rnd()), leftChild(nullptr), rightChild(nullptr) {}
+
+        static Node *getNewRoot() {
+            return nullptr;
+        }
+
+        static bool isEmptyTree(Node *node) {
+            return node == nullptr;
+        }
+
+        static void push(Node *node) {
+            if (isEmptyTree(node)) {
+                return;
+            }
+        }
+
+        static void update(Node *node) {
+            if (isEmptyTree(node)) {
+                return;
+            }
+        }
+
+        static Node *merge(Node *smaller, Node *bigger) {
+            if (isEmptyTree(smaller)) {
+                return bigger;
+            }
+            if (isEmptyTree(bigger)) {
+                return smaller;
+            }
+            push(smaller);
+            push(bigger);
+            Node *result;
+            if (smaller->y > bigger->y) {
+                result = smaller;
+                result->rightChild = merge(smaller->rightChild, bigger);
+            } else {
+                result = bigger;
+                result->leftChild = merge(smaller, bigger->leftChild);
+            }
+            update(result);
+            return result;
+        }
+
+        static pair<Node *, Node *> split(Node *node, int key) {
+            if (isEmptyTree(node)) {
+                return make_pair(getNewRoot(), getNewRoot());
+            }
+            push(node);
+            if (node->x <= key) {
+                auto rec = split(node->rightChild, key);
+                node->rightChild = rec.first;
+                update(node);
+                return make_pair(node, rec.second);
+            } else {
+                auto rec = split(node->leftChild, key);
+                node->leftChild = rec.second;
+                update(node);
+                return make_pair(rec.first, node);
+            }
+        }
+
+        static vector<Node *> multiSplit(Node *node, const initializer_list<int> &keys) {
+            vector<Node *> result = {node};
+            result.reserve(keys.size() + 1);
+            for (auto key : keys) {
+                auto p = split(result.back(), key);
+                result.back() = p.first;
+                result.push_back(p.second);
+            }
+            return result;
+        }
+
+        static Node *multiMerge(const initializer_list<Node *> nodes) {
+            auto result = getNewRoot();
+            for (auto node : nodes) {
+                result = merge(result, node);
+            }
+            return result;
+        }
     };
 
-    Node *getNewRoot() {
-        return nullptr;
-    }
-
-    bool isEmptyTree(Node *node) {
-        return node == nullptr;
-    }
-
-    void push(Node *node) {
-        if (isEmptyTree(node)) {
-            return;
-        }
-    }
-
-    void update(Node *node) {
-        if (isEmptyTree(node)) {
-            return;
-        }
-    }
-
-    Node *merge(Node *smaller, Node *bigger) {
-        if (isEmptyTree(smaller)) {
-            return bigger;
-        }
-        if (isEmptyTree(bigger)) {
-            return smaller;
-        }
-        push(smaller);
-        push(bigger);
-        Node *result;
-        if (smaller->y > bigger->y) {
-            result = smaller;
-            result->rightChild = merge(smaller->rightChild, bigger);
-        } else {
-            result = bigger;
-            result->leftChild = merge(smaller, bigger->leftChild);
-        }
-        update(result);
-        return result;
-    }
-
-    pair<Node *, Node *> split(Node *node, int key) {
-        if (isEmptyTree(node)) {
-            return make_pair(getNewRoot(), getNewRoot());
-        }
-        push(node);
-        if (node->x <= key) {
-            auto rec = split(node->rightChild, key);
-            node->rightChild = rec.first;
-            update(node);
-            return make_pair(node, rec.second);
-        } else {
-            auto rec = split(node->leftChild, key);
-            node->leftChild = rec.second;
-            update(node);
-            return make_pair(rec.first, node);
-        }
-    }
-
-    vector<Node *> multiSplit(Node *node, const initializer_list<int> &keys) {
-        vector<Node *> result = {node};
-        result.reserve(keys.size() + 1);
-        for (auto key : keys) {
-            auto p = split(result.back(), key);
-            result.back() = p.first;
-            result.push_back(p.second);
-        }
-        return result;
-    }
-
-    Node *multiMerge(const initializer_list<Node *> nodes) {
-        auto result = getNewRoot();
-        for (auto node : nodes) {
-            result = merge(result, node);
-        }
-        return result;
-    }
+    mt19937 Node::rnd(11);
 }
